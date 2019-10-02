b25b748ebb6758bd8db7b9a27245b823d6cf2563
Reilly Beacom <email@reillybeacom.net>
2019-10-02 11:29:18 +1000

    -

--- device/solar_hot_water_controller/solar_hot_water_controller.py
+++ device/solar_hot_water_controller/solar_hot_water_controller.py
@@ -1,21 +1,21 @@
 def logic(solar_collector, storage_tank, water_pump):
-    if solar_collector > 98:
+    if solar_collector >= 98:
         ## Too hot, don't want water to vaporise
         water_pump.off()
 
-    elif storage_tank > 60:
+    elif storage_tank >= 60:
         ## Water is hot enough
         water_pump.off()
 
     else:
         if water_pump.value():
             ## Pump is on
-            if solar_collector - storage_tank < 6:
+            if solar_collector - storage_tank <= 6:
                 ## Stop heating
                 water_pump.off()
 
         else:
             ## Pump is off
-            if solar_collector - storage_tank > 12:
+            if solar_collector - storage_tank >= 12:
                 ## Start heating
                 water_pump.on()
