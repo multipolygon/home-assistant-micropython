91efb39893e7964fb6f611ac4c7e2f4400de619c
Reilly Beacom <email@reillybeacom.net>
2019-10-02 11:52:50 +1000

    -

--- mqtt_listener.py
+++ mqtt_listener.py
@@ -3,18 +3,32 @@ import datetime
 import re
 import paho.mqtt.client as mqtt
 import secrets
+import json
 
 client = mqtt.Client("mqtt_forwarder")
 client.username_pw_set(secrets.MQTT_USER, password = secrets.MQTT_PASSWORD)
 
 def on_message(client, userdata, message):
-    print("[%s] %s: %s" % (str(datetime.datetime.now()), message.topic, message.payload.decode("utf-8")))
+    try:
+        print("")
+        print("[%s] %s%s:" % (str(datetime.datetime.now()), (message.retain and "{RETAIN} " or ""), message.topic))
+        print(
+            json.dumps(
+                json.loads(
+                    message.payload.decode("utf-8")
+                ),
+                sort_keys=True,
+                indent=4
+            )
+        )
+    except Exception as e:
+        print(e)
 
 client.on_message = on_message
 
 client.connect(secrets.MQTT_SERVER, port=secrets.MQTT_PORT)
 
-client.subscribe('#')
+client.subscribe(secrets.MQTT_USER + '/#')
 
 print('Running...')
 
