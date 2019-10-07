1f2efedd7746cbf347e02a275317721bc38f4ee0
Reilly Beacom <email@reillybeacom.net>
2019-10-08 10:26:24 +1100

    Updated to use new HA lib.

--- device/dual_probe_adc_thermometer/main.py
+++ device/dual_probe_adc_thermometer/main.py
@@ -1,13 +1,18 @@
 from machine import ADC, Pin
 from utime import sleep
-import pinmap
-import status_led
-import oled
-import secrets
-import wifi
-from umqtt_robust import MQTTClient
-import home_assistant
-import thermistor_adc
+
+from lib import secrets
+from lib.esp8266 import wifi
+from lib.esp8266.umqtt_robust import MQTTClient
+from lib.esp8266.wemos.d1mini import oled
+from lib.esp8266.wemos.d1mini import pinmap
+from lib.esp8266.wemos.d1mini import status_led
+
+from lib.home_assistant.binary_sensors.connectivity import ConnectivityBinarySensor
+from lib.home_assistant.sensors.signal_strength import SignalStrengthSensor
+from lib.home_assistant.sensors.temperature import TemperatureSensor
+
+import thermistor
 
 oled.write('POWER ON')
 
@@ -18,30 +23,28 @@ probe_2 = Pin(pinmap.D6, mode=Pin.OUT)
 state = {}
 
 mqtt = MQTTClient(
-    bytearray(home_assistant.MODEL + home_assistant.UID),
+    wifi.mac(),    
     secrets.MQTT_SERVER,
     user=bytearray(secrets.MQTT_USER),
     password=bytearray(secrets.MQTT_PASSWORD)
 )
 
-home_assistant.MAC = wifi.mac()
-home_assistant.TOPIC_PREFIX = secrets.MQTT_USER
+status_sensor = ConnectivityBinarySensor("Status", state, secrets.MQTT_USER)
+status_sensor.set_state(False)
+mqtt.set_last_will_json(status_sensor.state_topic(), state)
+status_sensor.set_state(True)
 
-ha_status = home_assistant.ConnectivityBinarySensor("Status", state)
-ha_status.set_state(False)
-mqtt.set_last_will_json(ha_status.state_topic(), state)
-
-ha_wifi_signal_strength = home_assistant.SignalStrengthSensor("WiFi Signal Strength", state)
-ha_temperature = home_assistant.TemperatureSensor('Temperature A', state)
-ha_temperature_2 = home_assistant.TemperatureSensor('Temperature B', state)
+wifi_signal_sensor = SignalStrengthSensor("WiFi Signal Strength", state, secrets.MQTT_USER)
+temperature_sensor_a = TemperatureSensor('Temperature A', state, secrets.MQTT_USER)
+temperature_sensor_b = TemperatureSensor('Temperature B', state, secrets.MQTT_USER)
 
 def mqtt_connected_callback():
     print('MQTT sending config...')
-    mqtt.publish_json(ha_status.config_topic(), ha_status.config(), retain=True)
-    ha_status.set_attributes({ "ip_address": wifi.ip(), "mac_address": wifi.mac() })
-    mqtt.publish_json(ha_wifi_signal_strength.config_topic(), ha_wifi_signal_strength.config(), retain=True)
-    mqtt.publish_json(ha_temperature.config_topic(), ha_temperature.config(), retain=True)
-    mqtt.publish_json(ha_temperature_2.config_topic(), ha_temperature_2.config(), retain=True)
+    mqtt.publish_json(status_sensor.config_topic(), status_sensor.config(), retain=True)
+    status_sensor.set_attributes({ "ip": wifi.ip(), "mac": wifi.mac() })
+    mqtt.publish_json(wifi_signal_sensor.config_topic(), wifi_signal_sensor.config(), retain=True)
+    mqtt.publish_json(temperature_sensor_a.config_topic(), temperature_sensor_a.config(), retain=True)
+    mqtt.publish_json(temperature_sensor_b.config_topic(), temperature_sensor_b.config(), retain=True)
     print('MQTT config sent.')
 
 mqtt.set_connected_callback(mqtt_connected_callback)
@@ -79,7 +82,7 @@ while True:
         oled.write(ip[0:8])
         oled.write(ip[8:])
         if mqtt.is_connected():
-            oled.write(home_assistant.UID)
+            oled.write(wifi.uid())
         else:
             oled.write('MQTT Err')
     else:
@@ -95,20 +98,19 @@ while True:
         adc1 = read_probe(1, loop)
         adc2 = read_probe(2, loop)
 
-        t1 = thermistor_adc.temperature(adc1)
-        t2 = thermistor_adc.temperature(adc2)
+        t1 = thermistor.temperature(adc1)
+        t2 = thermistor.temperature(adc2)
 
         oled.write('%4s%4s' % (wifi.is_connected() and wifi.rssi() or 'Err', mqtt.is_connected() and 'OK' or 'Err'), False)
+        oled.write('%4d%4d' % (adc1, adc2), False)
         oled.write('%4.0f%4.0f' % (t1, t2), False)
-        oled.write('', False)
         oled.write('', True)
 
         if loop % 10 == 0:
-            ha_status.set_state(True)
-            ha_wifi_signal_strength.set_state(wifi.rssi())
-            ha_temperature.set_state(round(t1, 2))
-            ha_temperature_2.set_state(round(t2, 2))
-            mqtt.publish_json(ha_status.state_topic(), state, reconnect=True)
+            wifi_signal_sensor.set_state(wifi.rssi())
+            temperature_sensor_a.set_state(round(t1, 2))
+            temperature_sensor_b.set_state(round(t2, 2))
+            mqtt.publish_json(status_sensor.state_topic(), state, reconnect=True) ## Note, all sensors share the same state topic.
             
         status_led.off()
 
