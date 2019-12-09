1cb63157b00f3dc29e84e1d129dd74fac6d8b3ec
Reilly Beacom <email@reillybeacom.net>
2019-12-09 18:33:22 +1100

    Timer crazy!

--- src/light/config.py
+++ src/light/config.py
@@ -5,8 +5,14 @@ from lib.esp8266.wemos.d1mini import pinmap
 ## Relay shield is D1 by default
 OUTPUT_PIN = pinmap.D1 
 
-# Register dimmable light with Home Assistant, subscribe to MQTT brightness commands and enable PWM on GPIO Pin:
-DIMMABLE = True
+## Register dimmable light with Home Assistant, subscribe to MQTT brightness commands and enable PWM on GPIO Pin:
+DIMMABLE = False
+
+## If DIMMABLE, set initial brightness between 0 and 1024
+INITIAL_BRIGHTNESS = 1024
+
+## Turn off light n minutes after being turned on, None will disable
+OFF_DELAY = 15 * 60 # seconds
 
 #### Button Switch ####
 
@@ -16,7 +22,12 @@ BUTTON_ENABLED = True
 BUTTON_PIN = pinmap.D3
 
 ## If True, a button press will toggle the output pin. Set to False to always turn ON and never OFF. False is useful for PIR sensors.
-BUTTON_TOGGLE = True
+BUTTON_TOGGLE = False
 
-## Set to False for PIR shield
-BUTTON_INVERTED = True
+## Set to True for 1-Button shield and False for PIR shield
+BUTTON_INVERTED = False
+
+#### Motion Sensor ###
+
+## If BUTTON_PIN is a PIR shield, send "button" press events over MQTT
+MOTION_SENSOR = True
