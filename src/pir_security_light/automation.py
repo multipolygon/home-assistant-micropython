d4b68efe0122a140f916667146e67e818f45eebb
Reilly Beacom <email@reillybeacom.net>
2019-12-16 16:30:09 +1100

    Automation as "state machine" GC: total: 37952, used: 34208, free: 3744

--- src/pir_security_light/automation.py
+++ src/pir_security_light/automation.py
@@ -2,51 +2,76 @@ from machine import Timer
 from micropython import schedule
 import config
 
-STANDBY, ON_AUTO, ON_MANUAL, DISABLED = range(4)
+STANDBY, ON_AUTO, ON_TIMER, ON_MANUAL, DISABLED = range(5)
+
+edges = {
+    STANDBY: {
+        'automatic': { False: DISABLED },
+        'light': { True: ON_MANUAL },
+        'motion': { True: ON_AUTO },
+    },
+    ON_AUTO: {
+        'automatic': { False: DISABLED },
+        'light': { False: STANDBY },
+        'motion': { False: ON_TIMER },
+    },
+    ON_TIMER: {
+        'automatic': { False: DISABLED },
+        'light': { False: STANDBY },
+        'motion': { True: ON_AUTO },
+    },
+    ON_MANUAL: {
+        'automatic': { False: DISABLED },
+        'light': { False: STANDBY },
+    },
+    DISABLED: {
+        'automatic': { True: STANDBY },
+    },
+}
 
 class Automation():
     def __init__(self, state):
         self.mode = STANDBY
-        self.timer = Timer(-1)
+        timer = Timer(-1)
 
-        def timeout(_):
-            state.set(light = False)
-            
-        def schedule_timeout(_):
-            schedule(timeout, None)
-
-        self.timeout = schedule_timeout
-
-    def on_automatic_on(self, state):
-        if self.mode == DISABLED:
-            self.mode = STANDBY
-
-    def on_automatic_off(self, state):
-        if self.mode == ON_AUTO:
-            self.timer.deinit()
-            state.set(light = False)
-        self.mode = DISABLED
-
-    def on_light_on(self, state):
-        if self.mode == STANDBY:
-            self.mode = ON_MANUAL
-
-    def on_light_off(self, state):
-        self.timer.deinit()
-        if self.mode != DISABLED:
-            self.mode = STANDBY
-
-    def on_motion_on(self, state):
-        self.timer.deinit()
-        if self.mode == STANDBY:
-            self.mode = ON_AUTO
+        def on_auto():
+            print('ON_AUTO')
+            timer.deinit()
             state.set(light = True)
+
+        def _timeout(_):
+            print('TIMEOUT')
+            state.set(light = False)
             
-    def on_motion_off(self, state):
-        self.timer.deinit()
-        if self.mode == ON_AUTO:
-            self.timer.init(
+        def _schedule_timeout(_):
+            schedule(_timeout, None)
+
+        def on_timer():
+            print('ON_TIMER')
+            timer.deinit()
+            timer.init(
                 period=config.MOTION_LIGHT_OFF_DELAY * 1000,
                 mode=Timer.ONE_SHOT,
-                callback=self.timeout
+                callback=_schedule_timeout
             )
+
+        def timer_deinit():
+            print('CLEAR')
+            timer.deinit()
+
+        self.modes = {
+            STANDBY: timer_deinit,
+            ON_AUTO: on_auto,
+            ON_TIMER: on_timer,
+            ON_MANUAL: timer_deinit,
+            DISABLED: timer_deinit,
+        }
+
+    def on_state_change(self, state, changed):
+        for edge in changed:
+            if edge in edges[self.mode]:
+                val = getattr(state, edge)
+                if val in edges[self.mode][edge]:
+                    self.mode = edges[self.mode][edge][val]
+                    self.modes[self.mode]()
+
