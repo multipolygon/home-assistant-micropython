ce671428f7e1b3b9a918d8a1618f9071e6a2ac91
Reilly Beacom <email@reillybeacom.net>
2019-12-18 19:15:30 +1100

    build

--- build/py/solar_hot_water_controller/lib/home_assistant/climate.py
+++ build/py/solar_hot_water_controller/lib/home_assistant/climate.py
@@ -2,9 +2,13 @@ from lib.home_assistant.main import HomeAssistant
 
 MODE_AUTO = "auto"
 MODE_OFF = "off"
+MODE_HEAT = "heat"
 
 class Climate(HomeAssistant):
     COMPONENT = "climate"
+
+    def set_mode(self, val):
+        return super().set_state(val, prop="mode")
 
     def set_current_temperature(self, val):
         return super().set_state(val, prop="temp")
@@ -22,16 +26,18 @@ class Climate(HomeAssistant):
         state_topic = self.state_topic()
         
         return {
-            "ret": retain,
-            "init": initial, # temperature
-            "curr_temp_t": state_topic,
-            "curr_temp_tpl": self.value_template(prop="temp"),
             "act_t": state_topic,
             "act_tpl": self.value_template(prop="act"),
-            "mode_cmd_t": self.mode_command_topic(),
-            "modes": [MODE_AUTO, MODE_OFF],
-            "temp_cmd_t": self.temperature_command_topic(),
-            "precision": 1.0,
-            "min_temp": min,
+            "curr_temp_t": state_topic,
+            "curr_temp_tpl": self.value_template(prop="temp"),
+            "init": initial, # temperature
             "max_temp": max,
+            "min_temp": min,
+            "mode_cmd_t": self.mode_command_topic(),
+            "mode_stat_t": state_topic,
+            "mode_stat_tpl": self.value_template(prop="mode"),
+            "modes": [MODE_AUTO, MODE_OFF, MODE_HEAT],
+            "precision": 1.0,
+            "ret": retain,
+            "temp_cmd_t": self.temperature_command_topic(),
         }
