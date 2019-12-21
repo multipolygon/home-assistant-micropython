3e6b123ef5a808c04222a8d8043762ca2eddd330
Reilly Beacom <email@reillybeacom.net>
2019-12-21 14:35:13 +1100

    Improvements to light

--- lib/home_assistant/light.py
+++ lib/home_assistant/light.py
@@ -1,5 +1,8 @@
 from lib.home_assistant.main import HomeAssistant
 
+CMD_KEY = "cmd"
+BRI_KEY = "bri"
+
 class Light(HomeAssistant):
     COMPONENT = "light"
     JSON_NAMESPACE = "lit"
@@ -7,13 +10,13 @@ class Light(HomeAssistant):
     PAYLOAD_OFF = STATE_OFF = "OFF"
 
     def set_state(self, val):
-        return super().set_state(self.STATE_ON if val else self.STATE_OFF, key = "sw")
+        return super().set_state(self.STATE_ON if val else self.STATE_OFF, key = CMD_KEY)
 
     def set_brightness_state(self, val):
-        return super().set_state(val, key = "bri")
+        return super().set_state(val, key = BRI_KEY)
 
     def brightness_command_topic(self):
-        return self.component_base_topic() + "/bri"
+        return "/".join((self.component_base_topic(), BRI_KEY))
 
     def component_config(self, optimistic=True, retain=True, brightness=False):
         config = {
@@ -21,7 +24,7 @@ class Light(HomeAssistant):
             "opt": optimistic,
             "ret": retain,
             "stat_t": self.state_topic(),
-            "stat_val_tpl": self.value_template(key = "sw"),
+            "stat_val_tpl": self.value_template(key = CMD_KEY),
         }
 
         if brightness:
@@ -29,7 +32,7 @@ class Light(HomeAssistant):
                 "bri_cmd_t": self.brightness_command_topic(),
                 "bri_scl": 100,
                 "bri_stat_t": self.state_topic(),
-                "bri_val_tpl": self.value_template(key = "bri"),
+                "bri_val_tpl": self.value_template(key = BRI_KEY),
             })
 
         return config
