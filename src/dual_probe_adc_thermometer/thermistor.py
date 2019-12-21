c14e3ee0145f3a17e8f0a2e7c183aa8b5a325437
Reilly Beacom <email@reillybeacom.net>
2019-12-21 11:42:02 +1100

    -

--- src/dual_probe_adc_thermometer/thermistor.py
+++
@@ -1,3 +0,0 @@
-def temperature(adc):
-    # https://www.wolframalpha.com/input/?i=linear+fit+%7B0%2C62%7D%2C%7B17.6%2C135%7D%2C%7B42.3%2C270%7D%2C%7B63%2C434%7D%2C%7B94.5%2C643%7D
-    return (adc - 36.1997) / 6.26956
