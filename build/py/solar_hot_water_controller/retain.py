3633fd2f0126972daada513dc245f88fa125ab08
Reilly Beacom <email@reillybeacom.net>
2019-12-28 17:54:23 +1100

    Add retain module (save/load JSON)

---
+++ build/py/solar_hot_water_controller/retain.py
@@ -0,0 +1,25 @@
+from uos import listdir
+from ujson import load, dump
+from config import RETAIN
+
+FN = 'retain.json'
+
+class Retain():
+    def __init__(self, state):
+        if FN in listdir():
+            with open(FN) as f:
+                obj = load(f)
+                for k in obj:
+                    if k not in RETAIN:
+                        del k
+                state.set(**obj)
+
+    def on_state_change(self, state, changed):
+        for k in RETAIN:
+            if k in changed:
+                obj = {}
+                for k in RETAIN:
+                    obj[k] = getattr(state, k)
+                with open(FN, 'w') as f:
+                    dump(obj, f)
+                break
