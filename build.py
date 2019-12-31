9e5a1a5e6b5929510ed31771b7c1e3cfbf68bcbe
Reilly Beacom <email@reillybeacom.net>
2019-12-31 12:19:12 +1100

    Improved build script `--secrets`, `--modified-only` and `--cross-compile` options.

--- build.py
+++ build.py
@@ -1,26 +1,37 @@
 from argparse import ArgumentParser
-from shutil import copyfile
+from shutil import copy2 as copyfile
+from shutil import copystat
 import mpy_cross
 import os, sys, subprocess, datetime, glob
 from platform import system
 import ast
+from time import sleep
 
-parser = ArgumentParser()
-parser.add_argument("--clean", action="store_true", help="Remove build files.")
-parser.add_argument("--modified-only", "-m", action="store_true", help="Only transfer modified files.")
-parser.add_argument("--build", "-b", action="store_true", help="Build only without calling mpfshell.")
-parser.add_argument("--transfer", "-t", action="store_true", help="Use mpfshell to copy files to device.")
-parser.add_argument("--cross-compile", "-x", action="store_true", help="Cross-compile (compress) to Micropython byte code.")
-parser.add_argument("--config", '-c', action="store", type=str, help='Use specific file in src/+/_config.')
-parser.add_argument("--secrets", '-s', action="store", type=str, help='Use specific file in lib/_secrets.')
-parser.add_argument("source", action="store", type=str, help="Src directory containing micropython main.py file.")
-args = parser.parse_args()
+def args():
+    p = ArgumentParser()
+    
+    p.add_argument("source", action="store", type=str, help="Directory containing micropython main.py file.")
+    
+    b = p.add_argument_group('build')
+    b.add_argument("--build", "-b", action="store_true", help="Build only without calling mpfshell.")
+    b.add_argument("--cross-compile", "-x", action="store_true", help="Cross-compile (compress) to Micropython byte code.")
+    b.add_argument("--config", '-c', action="store", type=str, help='Copy specified file as config.py')
+    b.add_argument("--clean", action="store_true", help="Remove build files.")
+    
+    t = p.add_argument_group('transfer')
+    t.add_argument("--transfer", "-t", action="store_true", help="Use mpfshell to copy files to device.")
+    t.add_argument("--modified-only", "-m", action="store_true", help="Only transfer modified files.")
+    t.add_argument("--secrets", '-s', action="store", type=str, help='Transfer specified file as secrets.py')
+    
+    return p.parse_args()
+
+args = args()
 
 base_dir = os.path.abspath(".")
 lib_dir = os.path.join(base_dir, "lib")
-source_dir = os.path.join(base_dir, "src", args.source)
+source_dir = os.path.abspath(args.source)
 main_file = os.path.join(source_dir, 'main.py')
-_build_dir_name = args.source + (("_%s" % args.config) if args.config else "")
+_build_dir_name = os.path.split(source_dir)[1] + (("_%s" % os.path.splitext(os.path.split(args.config)[1])[0]) if args.config else "")
 build_dir = os.path.join(base_dir, "build", "py", _build_dir_name)
 x_build_dir = os.path.join(base_dir, "build", "mpy", _build_dir_name)
 
@@ -70,41 +81,69 @@ def build():
     _copy_file(main_file, main_file.replace(source_dir, build_dir))
     _copy_dependencies(main_file, 2)
     if args.config:
-        config_file = os.path.join(source_dir, '_config', args.config + '.py')
-        _copy_file(config_file, os.path.join(build_dir, 'config.py'))
-        _copy_dependencies(config_file, 2)
-    if args.secrets:
-        secrets_file = os.path.join(base_dir, 'secrets', args.secrets + '.py')
-        _copy_file(secrets_file, os.path.join(build_dir, 'secrets.py'))
+        _copy_file(args.config, os.path.join(build_dir, 'config.py'))
+        _copy_dependencies(args.config, 2)
 
 def cross_compile():
-    for build_file in glob.iglob(os.path.join(build_dir, "**", "*"), recursive=True):
+    files = []
+    for build_file in glob.iglob(os.path.join(build_dir, "**", "*.py"), recursive=True):
         if os.path.isfile(build_file):
             target_file = build_file.replace(build_dir, x_build_dir).replace('.py', '.mpy')
-            target_dir, _ = os.path.split(target_file)
+            target_dir = os.path.split(target_file)[0]
             os.makedirs(target_dir, exist_ok=True)
             print(target_file)
             mpy_cross.run("-o", target_file, build_file)
+            files.append((build_file, target_file))
+    sleep(0.1)
+    for build_file, target_file in files:
+        copystat(build_file, target_file)
     copyfile(os.path.join(lib_dir, 'boot.py'), os.path.join(x_build_dir, 'boot.py'))
 
-def transfer():
-    port = os.path.split(list(glob.iglob("/dev/tty.usbserial-*"))[0])[-1]
+TIMESTAMP_FILE = '_timestamp.txt'
+TIMESTAMP_FILE_REMOTE = '_timestamp.remote.txt'
+    
+def _get_timestamp(port):
     commands = [
         "open %s" % port,
-        "lcd %s" % build_dir.replace(os.path.abspath("."), "."),
+        "get %s %s" % (TIMESTAMP_FILE, TIMESTAMP_FILE_REMOTE),
     ]
+    if os.path.isfile(TIMESTAMP_FILE_REMOTE):
+        os.remove(TIMESTAMP_FILE_REMOTE)
+    subprocess.call("mpfshell --noninteractive -c %s" % "\\; ".join(commands), shell=True)
+    if os.path.isfile(TIMESTAMP_FILE_REMOTE):
+        with open(TIMESTAMP_FILE_REMOTE) as f:
+            return float(f.read())
+    return 0
+
+def transfer():
+    build_dir = x_build_dir if args.cross_compile else build_dir
+    port = os.path.split(list(glob.iglob("/dev/tty.usbserial-*"))[0])[-1]
+    timestamp = _get_timestamp(port)
+    commands = ["open %s" % port]
+    if args.secrets:
+        commands.append("put %s secrets.py" % os.path.relpath(args.secrets))
+    commands.append("lcd %s" % os.path.relpath(build_dir))
+    newest = 0
     for build_file in glob.iglob(os.path.join(build_dir, "**", "*"), recursive=True):
-        if os.path.isfile(build_file):
-            build_file = build_file.replace(build_dir, "")
-            target_dir, target_file = os.path.split(build_file)
-            dir_path = ""
-            for sub_dir in target_dir.split('/'):
-                if sub_dir:
-                    dir_path = os.path.join(dir_path, sub_dir)
-                    cmd = "md %s" % dir_path
-                    if cmd not in commands:
-                        commands.append(cmd)
-            commands.append("put .%s" % build_file)
+        if os.path.isfile(build_file) and os.path.split(build_file)[1] != TIMESTAMP_FILE:
+            mtime = os.path.getmtime(build_file)
+            if mtime > newest:
+                newest = mtime
+            if not(args.modified_only) or mtime > timestamp:
+                build_file = build_file.replace(build_dir, "")
+                target_dir, target_file = os.path.split(build_file)
+                dir_path = ""
+                for sub_dir in target_dir.split('/'):
+                    if sub_dir:
+                        dir_path = os.path.join(dir_path, sub_dir)
+                        cmd = "md %s" % dir_path
+                        if cmd not in commands:
+                            commands.append(cmd)
+                commands.append("put .%s" % build_file)
+    if not(args.modified_only) or newest > timestamp:
+        with open(os.path.join(build_dir, TIMESTAMP_FILE), 'w') as f:
+            f.write(str(newest))
+        commands.append("put %s" % TIMESTAMP_FILE)
     commands.append("ls")
     commands.append("repl")
     for c in commands:
